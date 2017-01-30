ec112cddda428f8e83af59f08ccf759f3df67724
Unknown <jackhterzi@gmail.com>
2017-01-30 10:57:51 -0500

    First push for jumping jack
    Movement and jumping

---
+++ Jumpin Jak/Assets/Scripts/BounceMvmt.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceMvmt : MonoBehaviour {
+    public int jumpHeight;                  //bouncing
+    public float moveX;                     //moving forward
+    public bool isMovingLeft = false;       //check if the player is moving left to use in camera script
+
+
+    //public GameObject noBack;
+    //public GameObject floor;
+	
+
+
+	void Start () {
+     
+	}
+	
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            isMovingLeft = true;                            //changes is moving left
+            transform.position -= new Vector3(moveX, 0, 0);
+        }
+        else {
+            isMovingLeft = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(moveX, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D jump)
+    {
+        if (jump.gameObject.name != "backWall")
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpHeight;
+           
+        }
+    } 
+}
+
+
---
+++ Jumpin Jak/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+    public GameObject player;
+    BounceMvmt moveScript;
+    bool isFollowing = false;
+    float currentCameraX = 0;
+
+	// Use this for initialization
+	void Start () {
+        moveScript = player.GetComponent<BounceMvmt>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.transform.position.x >= currentCameraX && !isFollowing) {
+            isFollowing = true;
+        }
+
+        if (!moveScript.isMovingLeft && isFollowing)
+        {
+            Vector3 v = transform.position;
+            currentCameraX = player.transform.position.x;
+            v.x = currentCameraX;
+            transform.position = v;
+        }
+        else {
+            isFollowing = false;
+        }
+
+	}
+}
