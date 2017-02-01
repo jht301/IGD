6dacd732656181a4b355bf67d7aa1665b7bbe53d
Unknown <jackhterzi@gmail.com>
2017-02-01 12:18:41 -0500

    level 1 design + collectibles + counter
    Ya

---
+++ Jumpin Jak/Assets/Scripts/PartCollection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartCollection : MonoBehaviour {
+    
+    
+
+	void Start () {
+        
+	}
+     void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        obj.GetComponent<PlayerMngmt>().parts += 1;
+        
+       
+        Destroy(gameObject);
+        
+
+    } 
+}
---
+++ Jumpin Jak/Assets/Scripts/PlayerMngmt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMngmt : MonoBehaviour {
+    public int parts;
+    public string partCounter;
+    public Text Counter;
+    
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        partCounter = "Parts:" +parts + "/5";
+        Counter.text = partCounter;
+
+    }
+}
